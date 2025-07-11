38a0d341bb42026c2f7deaadd8659b8d7bfc43dc
JoaoAlmeid <jmalmeida700@gmail.com>
2025-07-11 16:52:24 -0300

    Ajuste no Logout

--- utils/logout.ts
+++ utils/logout.ts
@@ -1,10 +1,20 @@
-'use client'
-
-import { cookies } from "next/headers"
-import { redirect } from "next/navigation"
+import api from "./api"
 
 export async function logout() {
-  const cookieStore = await cookies()
-  cookieStore.delete('token')
-  redirect("/")
+  try {
+    await api.post('/admin/auth/sair', null, {
+      withCredentials: true
+    })
+
+    // Limpa cookies acessíveis via JS
+    document.cookie.split(';').forEach(cookie => {
+      const nome = cookie.split('=')[0].trim()
+      document.cookie = `${nome}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/; domain=.ccomfm.com.br`
+    })
+
+    // Redireciona
+    window.location.href = '/'
+  } catch (err) {
+    console.error('Erro ao sair do painel:', err)
+  }
 }
